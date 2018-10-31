36171d6a1e401a9d8057fc12e0668e4bf8644cf9
DaveL17 <dave.lehrer@icloud.com>
2018-10-30 19:29:07 -0500

    - Increments version number.

--- fantasticWeather.indigoPlugin/Contents/Server Plugin/plugin.py
+++ fantasticWeather.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -81,7 +81,7 @@ __copyright__ = Dave.__copyright__
 __license__   = Dave.__license__
 __build__     = Dave.__build__
 __title__     = "Fantastically Useful Weather Utility"
-__version__   = "0.1.09"
+__version__   = "0.1.10"
 
 # =============================================================================
 
